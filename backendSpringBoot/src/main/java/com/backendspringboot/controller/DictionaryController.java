9aa92c4c0ed7c43f21492de281f343f2908cef02
Baraniuk Bohdan <bodya_shark@mail.ru>
2024-02-24 22:00:56 +0200

    Add DictionaryController class

---
+++ backendSpringBoot/src/main/java/com/backendspringboot/controller/DictionaryController.java
@@ -0,0 +1,27 @@
+package com.backendspringboot.controller;
+
+import com.backendspringboot.service.DictionaryService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/dictionary")
+public class DictionaryController {
+
+private final DictionaryService dictionaryService;
+
+    public DictionaryController(DictionaryService dictionaryService) {
+        this.dictionaryService = dictionaryService;
+    }
+
+    @GetMapping("/checkWord/{word}")
+    @ResponseBody
+    public boolean checkWord(@PathVariable("word") String word) {
+        return dictionaryService.checkIfExist(word);
+    }
+
+    @GetMapping("/getFirstWord")
+    @ResponseBody
+    public String getFirstWord() {
+        return dictionaryService.getFirstWord();
+    }
+}
